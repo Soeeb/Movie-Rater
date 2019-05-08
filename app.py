a64e1e46be043032af287014c32133ec0ad2db00
Daniel <dandeng24@live.com>
2019-05-08 17:52:56 +1200

    v2.2
    full functionality, no validation present.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 import os, sqlite3
 app = Flask(__name__)
 path = os.getcwd()+"/static/images"
 wsgi_app = app.wsgi_app
+app.config['UPLOAD_FOLDER'] = path 
 
 def export_filename_database(c, conn):
     with conn:
@@ -38,6 +40,14 @@ def import_review(c, conn, filename):
         output = c.fetchone()
         return output[0]
 
+def import_upload(c, conn, filename):
+    with conn:
+        c.execute("INSERT INTO movies (filename) VALUES (?)", (filename,))
+
+def delete(c, conn, filename):
+    c.execute('DELETE FROM movies WHERE filename=?',(filename,))
+    os.remove(path + "/" + filename)
+
 def import_all(c, conn):
     class Movie:
         def __init__(self, filename, rating, review):
@@ -65,7 +75,7 @@ c.execute("""CREATE TABLE movies (
             rating inter,
             review text)
             """)  '''
-            
+       
 @app.route('/')
 def main():
     conn = sqlite3.connect(os.getcwd() +'/static/movie_database.db')
@@ -79,20 +89,37 @@ def main():
 def rateMain():
     conn = sqlite3.connect(os.getcwd() +'/static/movie_database.db')
     c = conn.cursor()
-    print (import_all(c, conn))
     filename = request.args.get("imageName")
     rating = import_rating(c, conn, filename)
     review = import_review(c, conn, filename)
     if request.method == "POST":
-        form = request.form
-        for movie in import_all(c, conn):
-            if movie.filename == filename:
-                export_rating(filename, int(form["rating"]), c, conn)
-                rating = int(form["rating"])
-                export_review(c, conn, form["review"], filename)
-                review = form["review"]
+        if request.form.get('submit') == 'submit':
+            form = request.form
+            for movie in import_all(c, conn):
+                if movie.filename == filename:
+                    export_rating(filename, int(form["rating"]), c, conn)
+                    rating = int(form["rating"])
+                    export_review(c, conn, form["review"], filename)
+                    review = form["review"]
+        elif request.form.get('delete') == 'delete':
+            delete(c, conn, filename)
+            close(c, conn)
+            return redirect(url_for('main'))
     close(c, conn)
     return render_template("rate.html", rating = rating, review = review)
+
+@app.route('/upload', methods = ["POST", "GET"])
+def uploadMain():
+    conn = sqlite3.connect(os.getcwd() +'/static/movie_database.db')
+    c = conn.cursor()
+    uploaded = "No file uploaded"
+    if request.method == "POST":
+       file = request.files["file"]
+       filename = secure_filename(file.filename)
+       file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+       import_upload(c, conn, filename)
+       uploaded = "File uploaded succesfully!"
+    return render_template("upload.html", uploaded = uploaded)
     
 if __name__ == '__main__':
     import os
