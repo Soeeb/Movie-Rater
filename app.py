6e6122d057a2f4589d5c2acce8cb9e7f42d321b2
Daniel <dandeng24@live.com>
2019-05-15 08:49:56 +1200

    V2.2
    added fcuntions for more values to use

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 import os, sqlite3
 app = Flask(__name__)
+app.secret_key = 'very_secret'
 path = os.getcwd()+"/static/images"
 wsgi_app = app.wsgi_app
 app.config['UPLOAD_FOLDER'] = path 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 def export_filename_database(c, conn):
     with conn:
@@ -62,19 +64,67 @@ def import_all(c, conn):
             temp_list.append(Movie(temp[0], temp[1], temp[2]))
         return temp_list
 
+def export_year(c, conn, year, filename):
+    with conn:
+        c.execute("UPDATE movies SET year=? WHERE filename=?",(year, filename))
+
+def import_year(c, conn, filename):
+    with conn:
+        c.execute("SELECT year FROM movies WHERE filename=?", (filename,))
+        output = c.fetchone()
+        return output[0]
+
+def export_director(c, conn, director, filename):
+    with conn:
+        c.execute("UPDATE movies SET director=? WHERE filename=?",(director, filename))
+
+def import_director(c, conn, filename):
+    with conn:
+        c.execute("SELECT director FROM movies WHERE filename=?", (filename,))
+        output = c.fetchone()
+        return output[0]
+    
+def export_studio(c, conn, studio, filename):
+    with conn:
+        c.execute("UPDATE movies SET studio=? WHERE filename=?",(studio, filename))
+
+def import_studio(c, conn, filename):
+    with conn:
+        c.execute("SELECT studio FROM movies WHERE filename=?", (filename,))
+        output = c.fetchone()
+        return output[0]
+
+def export_title(c, conn, title, filename):
+    with conn:
+        c.execute("UPDATE movies SET title=? WHERE filename=?",(title, filename))
+
+def import_title(c, conn, filename):
+    with conn:
+        c.execute("SELECT title FROM movies WHERE filename=?", (filename,))
+        output = c.fetchone()
+        return output[0]
+
 def close(c, conn):
     conn.commit()
     conn.close()
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 #used during first time to create a table
-'''
+
 conn = sqlite3.connect(os.getcwd() +'/static/movie_database.db')
 c = conn.cursor()
 c.execute("""CREATE TABLE movies (
             filename text,
             rating inter,
-            review text)
-            """)  '''
+            review text
+            year inter,
+            director text,
+            studio text,
+            title text)
+            """)  
        
 @app.route('/')
 def main():
@@ -101,9 +151,11 @@ def rateMain():
                     rating = int(form["rating"])
                     export_review(c, conn, form["review"], filename)
                     review = form["review"]
+                    flash("Movie reviewed successfully!", "success")
         elif request.form.get('delete') == 'delete':
             delete(c, conn, filename)
             close(c, conn)
+            flash("File deleted successfully!", "success")
             return redirect(url_for('main'))
     close(c, conn)
     return render_template("rate.html", rating = rating, review = review)
@@ -112,14 +164,22 @@ def rateMain():
 def uploadMain():
     conn = sqlite3.connect(os.getcwd() +'/static/movie_database.db')
     c = conn.cursor()
-    uploaded = "No file uploaded"
     if request.method == "POST":
-       file = request.files["file"]
-       filename = secure_filename(file.filename)
-       file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-       import_upload(c, conn, filename)
-       uploaded = "File uploaded succesfully!"
-    return render_template("upload.html", uploaded = uploaded)
+        if 'file' not in request.files:
+            flash('No file extension!', 'warning')
+            return redirect(request.url)
+        file = request.files["file"]
+        if file.filename == "":
+            flash('No file selected!', 'warning')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            import_upload(c, conn, filename)
+            flash("File has been uploaded successfully!", "success")
+        else:
+            flash('Incorrect file type!', 'warning')
+    return render_template("upload.html")
     
 if __name__ == '__main__':
     import os
